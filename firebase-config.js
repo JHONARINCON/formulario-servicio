93ec13a6710363c25fe088331c9dac58f9ffd1bf
JHONARINCON <jhonalexrinconr@gmail.com>
2025-06-04 00:23:40 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBaJ7lhGoCTq9a4cmL5P31b9RbF31QaNnE",
-  authDomain: "ordenes-servicio-a8209.firebaseapp.com",
+  authDomain: "https://ordenes-servicio-a8209-default-rtdb.firebaseio.com/",
   projectId: "ordenes-servicio-a8209",
   storageBucket: "ordenes-servicio-a8209.firebasestorage.app",
   messagingSenderId: "480144746551",

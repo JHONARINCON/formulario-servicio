7cada7993ba009550c76e74ad84c08d9b14b57e0
JHONARINCON <jhonalexrinconr@gmail.com>
2025-06-03 23:57:51 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBaJ7lhGoCTq9a4cmL5P31b9RbF31QaNnE",
+  authDomain: "ordenes-servicio-a8209.firebaseapp.com",
+  projectId: "ordenes-servicio-a8209",
+  storageBucket: "ordenes-servicio-a8209.firebasestorage.app",
+  messagingSenderId: "480144746551",
+  appId: "1:480144746551:web:28787185ca77baa50737b2"
+};

42ece81a3cd0fb2469330dcff570203a68e9a818
JHONARINCON <jhonalexrinconr@gmail.com>
2025-06-04 01:39:39 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,9 @@
-// firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-lite.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBaJ7lhGoCTq9a4cmL5P31b9RbF31QaNnE",
   authDomain: "ordenes-servicio-a8209.firebaseapp.com",
@@ -11,8 +13,5 @@ const firebaseConfig = {
   appId: "1:480144746551:web:28787185ca77baa50737b2"
 };
 
-// Inicializar Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializar Firestore Lite y exportar
-export const db = getFirestore(app);

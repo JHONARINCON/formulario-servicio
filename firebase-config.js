326ebd588ddbe58711f103ce453d21311ff44833
JHONARINCON <jhonalexrinconr@gmail.com>
2025-06-04 01:34:39 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBaJ7lhGoCTq9a4cmL5P31b9RbF31QaNnE",
   authDomain: "ordenes-servicio-a8209.firebaseapp.com",
   projectId: "ordenes-servicio-a8209",
-  storageBucket: "ordenes-servicio-a8209.appspot.com",
+  storageBucket: "ordenes-servicio-a8209.firebasestorage.app",
   messagingSenderId: "480144746551",
   appId: "1:480144746551:web:28787185ca77baa50737b2"
 };

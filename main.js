7ae01025096b9348ba539e2720584d3d68d591ad
JHONARINCON <jhonalexrinconr@gmail.com>
2025-06-04 03:11:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { db } from './firebaseConfig.js';
+import { db } from './firebase-config.js';
 import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-lite.js";
 import jsPDF from "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js";
 
